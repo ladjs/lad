5cdf14c641b542b4ab7fb6d07e8e1268a6b9ab37
Nick Baugh <niftylettuce@gmail.com>
2014-10-09 17:55:30 -0400

    Added custom font mixin, fixed asset loading for CSS (because its nice to not have to @import all the Bootstrap LESS files such as mixins)

--- assets/public/js/main.js
+++ assets/public/js/main.js
@@ -2,10 +2,6 @@
   'use strict';
 
   $(function() {
-
-    $('body').css('background', 'transparent url(\'/img/igloo.png\') top right no-repeat');
-    console.log('TODO: change this file.');
-
   });
 
 }(window, jQuery));
