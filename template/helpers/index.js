280044493fc193f90c45e9e82e7cb97b3bbe84c0
Alexis Tyler <xo@wvvw.me>
2017-09-10 14:09:50 +0930

    switch renderPage to koa-views-render

--- template/helpers/index.js
+++ template/helpers/index.js
@@ -1,11 +1,11 @@
 const Logger = require('@ladjs/logger');
 const Timeout = require('koa-better-timeout');
+const render = require('koa-views-render');
 
 const graceful = require('./graceful');
 const meta = require('./meta');
 const passport = require('./passport');
 const dynamicViewHelpers = require('./dynamic-view-helpers');
-const renderPage = require('./render-page');
 const policies = require('./policies');
 const i18n = require('./i18n');
 const _404Handler = require('./_404-handler');
@@ -19,7 +19,7 @@ module.exports = {
   Timeout,
   passport,
   dynamicViewHelpers,
-  renderPage,
+  renderPage: render,
   policies,
   logger: new Logger(),
   i18n,
