c04a2cb1d2e64daa3c7206eecfb6278c4d3df029
Alexis Tyler <xo@wvvw.me>
2017-09-21 13:01:21 +0930

    silence logging while running tests

--- template/config/environments/test.js
+++ template/config/environments/test.js
@@ -1,4 +1,8 @@
 // turn off max length eslint rule since this is a config file with long strs
 /* eslint max-len: 0 */
 
-module.exports = {};
+module.exports = {
+  logger: {
+    silent: true
+  }
+};
