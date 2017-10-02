0a952569190beba59fb81c9ff8e68a187a547cae
Alexis Tyler <xo@wvvw.me>
2017-10-02 12:35:32 +1030

    add error for missing body

--- template/app/controllers/web/auth.js
+++ template/app/controllers/web/auth.js
@@ -96,6 +96,9 @@ const login = async (ctx, next) => {
 const register = async ctx => {
   const { body } = ctx.request;
 
+  if (Object.keys(body).length === 0)
+    return ctx.throw(Boom.badData(ctx.translate('MISSING_REGISTER_FIELDS')));
+
   if (!_.isString(body.email) || !validator.isEmail(body.email))
     return ctx.throw(Boom.badRequest(ctx.translate('INVALID_EMAIL')));
 
