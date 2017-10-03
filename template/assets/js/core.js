7cdfcc1f30b8fb1bb310acad0bd1732cf935413c
Nick Baugh <niftylettuce@gmail.com>
2017-10-03 12:46:21 -0400

    Fixed issue found by @pablopunk with gulpfile, Fixed deps in wrong folder

--- template/assets/js/core.js
+++ template/assets/js/core.js
@@ -436,41 +436,43 @@ const api = new Frisbee({
     $('[data-toggle="popover"]').popover();
 
     // Handle custom file inputs
-    $(
-      'body'
-    ).on('change', 'input[type="file"][data-toggle="custom-file"]', function() {
-      const $input = $(this);
-      const target = $input.data('target');
-      const $target = $(target);
+    $('body').on(
+      'change',
+      'input[type="file"][data-toggle="custom-file"]',
+      function() {
+        const $input = $(this);
+        const target = $input.data('target');
+        const $target = $(target);
 
-      if ($target.length === 0)
-        return console.error('Invalid target for custom file', $input);
+        if ($target.length === 0)
+          return console.error('Invalid target for custom file', $input);
 
-      if (!$target.attr('data-content'))
-        return console.error(
-          'Invalid `data-content` for custom file target',
-          $input
-        );
+        if (!$target.attr('data-content'))
+          return console.error(
+            'Invalid `data-content` for custom file target',
+            $input
+          );
 
-      // Set original content so we can revert if user deselects file
-      if (!$target.attr('data-original-content'))
-        $target.attr('data-original-content', $target.attr('data-content'));
+        // Set original content so we can revert if user deselects file
+        if (!$target.attr('data-original-content'))
+          $target.attr('data-original-content', $target.attr('data-content'));
 
-      const input = $input.get(0);
+        const input = $input.get(0);
 
-      let name =
-        _.isObject(input) &&
-        _.isObject(input.files) &&
-        _.isObject(input.files[0]) &&
-        _.isString(input.files[0].name)
-          ? input.files[0].name
-          : $input.val();
+        let name =
+          _.isObject(input) &&
+          _.isObject(input.files) &&
+          _.isObject(input.files[0]) &&
+          _.isString(input.files[0].name)
+            ? input.files[0].name
+            : $input.val();
 
-      if (_.isNull(name) || name === '')
-        name = $target.attr('data-original-content');
+        if (_.isNull(name) || name === '')
+          name = $target.attr('data-original-content');
 
-      $target.attr('data-content', name);
-    });
+        $target.attr('data-content', name);
+      }
+    );
 
     // Handle clipboard copy helper buttons
     if (Clipboard.isSupported()) {
