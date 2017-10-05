7b46a466bbfe1b69505bd3fa5ed7072bf3c5a285
Alexis Tyler <xo@wvvw.me>
2017-10-05 14:43:35 +1030

    add more password reset tests

--- template/test/web/auth.js
+++ template/test/web/auth.js
@@ -49,7 +49,7 @@ test('fails registering invalid email', async t => {
 
 test(`doesn't leak used email`, async t => {
   const email = 'test2@example.com';
-  const password = '!@K#NLK!#N';
+  const password = '!@K#NLK!#NSADKMSAD:K';
 
   await koaRequest(app)
     .post('/en/signup')
@@ -183,7 +183,7 @@ test(`fails resetting password for non-existant user`, async t => {
   t.is(res.body.message, 'Reset token and email were not valid together.');
 });
 
-test(`fails resetting password with invalid token`, async t => {
+test(`fails resetting password with invalid reset_token`, async t => {
   const email = 'test8@example.com';
   const password = '!@K#NLK!#N';
 
@@ -206,6 +206,147 @@ test(`fails resetting password with invalid token`, async t => {
 
   t.is(res.status, 400);
   t.is(res.body.message, 'Reset token and email were not valid together.');
+});
+
+test(`fails resetting password with missing new password`, async t => {
+  const email = 'test9@example.com';
+  const password = '!@K#NLK!#N';
+
+  await koaRequest(app)
+    .post('/en/signup')
+    .set('Accept', 'application/json')
+    .send({ email })
+    .send({ password });
+
+  await koaRequest(app)
+    .post('/en/forgot-password')
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  const user = await Users.findOne({ email })
+    .select('reset_token')
+    .exec();
+
+  const res = await koaRequest(app)
+    .post(`/en/reset-password/${user.reset_token}`)
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  t.is(res.status, 400);
+  t.is(res.body.message, 'Password was invalid.');
+});
+
+test(`fails resetting password with invalid email`, async t => {
+  const email = 'test10@example.com';
+  const password = '!@K#NLK!#N';
+
+  await koaRequest(app)
+    .post('/en/signup')
+    .set('Accept', 'application/json')
+    .send({ email })
+    .send({ password });
+
+  await koaRequest(app)
+    .post('/en/forgot-password')
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  const user = await Users.findOne({ email })
+    .select('reset_token')
+    .exec();
+
+  const res = await koaRequest(app)
+    .post(`/en/reset-password/${user.reset_token}`)
+    .set('Accept', 'application/json')
+    .send({ email: 'wrongemail' });
+
+  t.is(res.status, 400);
+  t.is(res.body.message, 'Email address was invalid.');
+});
+
+test(`fails resetting password with invalid email + reset_token match`, async t => {
+  const email = 'test11@example.com';
+  const password = '!@K#NLK!#N';
+
+  await koaRequest(app)
+    .post('/en/signup')
+    .set('Accept', 'application/json')
+    .send({ email })
+    .send({ password });
+
+  await koaRequest(app)
+    .post('/en/forgot-password')
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  const user = await Users.findOne({ email })
+    .select('reset_token')
+    .exec();
+
+  const res = await koaRequest(app)
+    .post(`/en/reset-password/${user.token}`)
+    .set('Accept', 'application/json')
+    .send({ email: 'wrongemail@example.com' })
+    .send({ password });
+
+  t.is(res.status, 400);
+  t.is(res.body.message, 'Reset token and email were not valid together.');
+});
+
+test(`fails resetting password if new password is too weak`, async t => {
+  const email = 'test12@example.com';
+  const password = '!@K#NLK!#N';
+
+  await koaRequest(app)
+    .post('/en/signup')
+    .set('Accept', 'application/json')
+    .send({ email })
+    .send({ password });
+
+  await koaRequest(app)
+    .post('/en/forgot-password')
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  const user = await Users.findOne({ email })
+    .select('reset_token')
+    .exec();
+
+  const res = await koaRequest(app)
+    .post(`/en/reset-password/${user.reset_token}`)
+    .set('Accept', 'application/json')
+    .send({ email })
+    .send({ password: 'password' });
+
+  t.is(res.status, 400);
+  t.is(res.body.message, 'Password strength was not strong enough.');
+});
+
+test(`fails resetting password if reset was already tried in the last 30 mins`, async t => {
+  const email = 'test12@example.com';
+  const password = '!@K#NLK!#N';
+
+  await koaRequest(app)
+    .post('/en/signup')
+    .set('Accept', 'application/json')
+    .send({ email })
+    .send({ password });
+
+  await koaRequest(app)
+    .post('/en/forgot-password')
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  const res = await koaRequest(app)
+    .post('/en/forgot-password')
+    .set('Accept', 'application/json')
+    .send({ email });
+
+  t.is(res.status, 400);
+  t.is(
+    res.body.message,
+    'You can only request a password reset every 30 minutes.  Please try again in 30 minutes.'
+  );
 });
 
 // @TODO: Add test for each provider
