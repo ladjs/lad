9fe640104f93ae76fd1686c6f0ace4212d3739e0
niftylettuce <niftylettuce@gmail.com>
2017-11-26 07:43:16 -0500

    Added @ladjs/web and @ladjs/api

--- template/proxy.js
+++ template/proxy.js
@@ -1,14 +1,5 @@
-const http = require('http');
-const url = require('url');
+#!/usr/bin/env node
+const proxy = require('@ladjs/proxy');
 
-const app = http.createServer((req, res) => {
-  res.writeHead(301, {
-    Location: url.parse(`https://${req.headers.host}${req.url}`).href
-  });
-
-  res.end();
-});
-
-if (!module.parent) app.listen(80);
-
-module.exports = app;
+if (!module.parent) proxy.listen(80);
+module.exports = proxy;
--- template/routes/web/auth.js
+++ template/routes/web/auth.js
@@ -1,5 +1,6 @@
 const Boom = require('boom');
 const Router = require('koa-router');
+const boolean = require('boolean');
 
 const { policies, passport } = require('../../helpers');
 const config = require('../../config');
@@ -8,15 +9,27 @@ const router = new Router({
   prefix: '/auth'
 });
 
+const catchError = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    if (ctx.params.provider === 'google' && err.message === 'Consent required')
+      return ctx.redirect('/auth/google/consent');
+    ctx.flash('error', err.message);
+    ctx.redirect('/login');
+  }
+};
+
 router
   .param('provider', (provider, ctx, next) => {
-    if (!config.auth.providers[provider]) return ctx.throw(Boom.badRequest('Invalid provider'));
+    if (!boolean(process.env[`AUTH_${provider.toUpperCase()}_ENABLED`]))
+      return ctx.throw(Boom.badRequest('Invalid provider'));
     return next();
   })
-  .get('/:provider', policies.ensureLoggedOut, config.auth.catchError, (ctx, next) =>
+  .get('/:provider', policies.ensureLoggedOut, catchError, (ctx, next) =>
     passport.authenticate(ctx.params.provider, config.auth[ctx.params.provider])(ctx, next)
   )
-  .get('/:provider/ok', policies.ensureLoggedOut, config.auth.catchError, (ctx, next) => {
+  .get('/:provider/ok', policies.ensureLoggedOut, catchError, (ctx, next) => {
     const redirect = ctx.session.returnTo
       ? ctx.session.returnTo
       : `/${ctx.req.locale}${config.auth.callbackOpts.successReturnToOrRedirect}`;
@@ -26,15 +39,15 @@ router
     })(ctx, next);
   });
 
-if (config.auth.providers.google)
+if (boolean(process.env.AUTH_GOOGLE_ENABLED))
   router.get(
     '/google/consent',
     policies.ensureLoggedOut,
-    config.auth.catchError,
+    catchError,
     passport.authenticate('google', {
       accessType: 'offline',
       prompt: 'consent', // see google strategy in passport helper
-      scope: config.auth.google.scope
+      scope: []
     })
   );
 
