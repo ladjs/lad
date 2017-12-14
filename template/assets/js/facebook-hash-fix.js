c1c6214793340cac264bbe7acb58b0b8f0b43d53
niftylettuce <niftylettuce@gmail.com>
2017-12-14 04:40:02 -0500

    Upgraded deps, Used shorthand l method for links, Fixed localization for URLs

--- template/assets/js/facebook-hash-fix.js
+++ template/assets/js/facebook-hash-fix.js
@@ -1,10 +1,10 @@
 // Remove the ugly Facebook appended hash
 // <https://github.com/jaredhanson/passport-facebook/issues/12>
-(() => {
+module.exports = () => {
   if (window.location.hash !== '' && window.location.hash !== '#_=_') return;
   window.history.replaceState(
     undefined,
     undefined,
     window.location.pathname + window.location.search
   );
-})();
+};
