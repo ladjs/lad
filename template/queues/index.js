0ec0c57977570641b2815b9e50f1644cbc8fdbe5
niftylettuce <niftylettuce@gmail.com>
2020-06-30 23:44:38 -0500

    fix: fixed tests (temporarily removed mandarin/markdown until lad v2 released)

--- template/queues/index.js
+++ template/queues/index.js
@@ -31,40 +31,6 @@ const queues = [
         concurrency: 1
       }
     ]
-  },
-  {
-    name: 'translate-phrases',
-    options: {
-      attempts: 1,
-      defaultJobOptions: {
-        repeat: {
-          every: process.env.NODE_ENV === 'production' ? ms('15s') : ms('15m')
-        }
-      }
-    },
-    processors: [
-      {
-        processor: path.join(__dirname, 'translate-phrases.js'),
-        concurrency: 1
-      }
-    ]
-  },
-  {
-    name: 'translate-markdown',
-    options: {
-      attempts: 1,
-      defaultJobOptions: {
-        repeat: {
-          every: process.env.NODE_ENV === 'production' ? ms('30s') : ms('30m')
-        }
-      }
-    },
-    processors: [
-      {
-        processor: path.join(__dirname, 'translate-markdown.js'),
-        concurrency: 1
-      }
-    ]
   }
 ];
 
