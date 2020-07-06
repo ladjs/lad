8b40a56176c51e6961cf1565f7ff505af0438952
shadowgate15 <taylorschley25@gmail.com>
2020-07-06 09:02:01 -0500

    test: add sinon and refactor tests

--- template/test/web/auth.js
+++ template/test/web/auth.js
@@ -2,7 +2,10 @@
 const util = require('util');
 const test = require('ava');
 const cryptoRandomString = require('crypto-random-string');
+const sinon = require('sinon');
+const { factory } = require('factory-girl');
 
+const Boom = require('@hapi/boom');
 const phrases = require('../../config/phrases');
 const config = require('../../config');
 const { Users } = require('../../app/models');
@@ -14,8 +17,18 @@ test.after.always(after);
 test.beforeEach(beforeEach);
 test.afterEach.always(afterEach);
 
-test.serial('creates new user', async t => {
+test('creates new user', async t => {
   const { web } = t.context;
+  const user = [
+    {
+      id: 1,
+      email: 'lordbyron@example.com',
+      password: 'something'
+    }
+  ];
+  t.context.register = sinon.stub(Users, 'register').resolves(user);
+  t.context.serialize.yields(null, { id: 1 });
+
   const res = await web.post('/en/register').send({
     email: 'lordbyron@example.com',
     password: '?X#8Hn=PbkvTD/{'
@@ -24,13 +37,14 @@ test.serial('creates new user', async t => {
   t.is(res.header.location, '/en/dashboard');
   t.is(res.status, 302);
 
-  // make sure user was added to database
-  const newUser = await Users.findOne({ email: 'lordbyron@example.com' });
-  t.is(newUser.email, 'lordbyron@example.com');
+  t.context.register.restore();
 });
 
 test('fails registering with easy password', async t => {
   const { web } = t.context;
+  t.context.register = sinon.stub(Users, 'register').throws(
+    Boom.badRequest(phrases.INVALID_PASSWORD_STRENGTH));
+
   const res = await web.post('/en/register').send({
     email: 'emilydickinson@example.com',
     password: 'password'
@@ -39,13 +53,21 @@ test('fails registering with easy password', async t => {
   t.is(res.body.message, phrases.INVALID_PASSWORD_STRENGHT);
   t.is(res.status, 400);
 
-  // make sure user was not added to database
-  const newUser = await Users.findOne({ email: 'emilydickinson@example.com' });
-  t.is(newUser, null);
+  t.context.register.restore();
 });
 
 test('successfully registers with strong password', async t => {
   const { web } = t.context;
+  const user = [
+    {
+      id: 1,
+      email: 'test12@example.com',
+      password: 'Thi$i$@$r0ng3rP@$$W0rdMyDude'
+    }
+  ];
+  t.context.register = sinon.stub(Users, 'register').resolves(user);
+  t.context.serialize.yields(null, { id: 1 });
+
   const res = await web.post('/en/register').send({
     email: 'test12@example.com',
     password: 'Thi$i$@$r0ng3rP@$$W0rdMyDude'
@@ -55,12 +77,21 @@ test('successfully registers with strong password', async t => {
   t.is(res.header.location, '/en/dashboard');
   t.is(res.status, 302);
 
-  const newUser = await Users.findOne({ email: 'test12@example.com' });
-  t.is(newUser.email, 'test12@example.com');
+  t.context.register.restore();
 });
 
 test('successfully registers with stronger password', async t => {
   const { web } = t.context;
+  const user = [
+    {
+      id: 1,
+      email: 'test123@example.com',
+      password: cryptoRandomString({ length: 50 })
+    }
+  ];
+  t.context.register = sinon.stub(Users, 'register').resolves(user);
+  t.context.serialize.yields(null, { id: 1 });
+
   const res = await web.post('/en/register').send({
     email: 'test123@example.com',
     password: cryptoRandomString({ length: 50 })
@@ -70,8 +101,7 @@ test('successfully registers with stronger password', async t => {
   t.is(res.header.location, '/en/dashboard');
   t.is(res.status, 302);
 
-  const newUser = await Users.findOne({ email: 'test123@example.com' });
-  t.is(newUser.email, 'test123@example.com');
+  t.context.register.restore();
 });
 
 test('fails registering invalid email', async t => {
@@ -87,106 +117,142 @@ test('fails registering invalid email', async t => {
 
 test("doesn't leak used email", async t => {
   const { web } = t.context;
-  const email = 'test2@example.com';
-  const password = '!@K#NLK!#NSADKMSAD:K';
-
-  await web.post('/en/register').send({ email, password });
-  await web.get('/en/logout');
+  const user = [
+    {
+      id: 1,
+      email: 'test2@example.com',
+      password: '!@K#NLK!#NSADKMSAD:K'
+    }
+  ];
+  t.context.findByUsername = sinon.stub(Users, 'findByUsername').resolves(user);
 
   const res = await web.post('/en/register')
     .send({
-      email,
+      email: 'test2@example.com',
       password: 'wrongpassword'
     });
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.PASSPORT_USER_EXISTS_ERROR);
+
+  t.context.findByUsername.restore();
 });
 
 test('allows password reset for valid email (HTML)', async t => {
   const { web } = t.context;
-  const email = 'test3@example.com';
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  const user = await factory.build('user');
+  t.context.findOne = sinon.stub(Users, 'findOne')
+    .resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   const res = await web
     .post('/en/forgot-password')
     .set({ Accept: 'text/html' })
-    .send({ email });
+    .send({ email: user.email });
 
   t.is(res.status, 302);
   t.is(res.header.location, '/');
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('allows password reset for valid email (JSON)', async t => {
   const { web } = t.context;
-  const email = 'test4@example.com';
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  const user = await factory.build('user');
+  t.context.findOne = sinon.stub(Users, 'findOne')
+    .resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
-  const res = await web.post('/en/forgot-password').send({ email });
+  const res = await web.post('/en/forgot-password')
+    .send({ email: user.email });
 
   t.is(res.status, 302);
   t.is(res.header.location, '/');
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('resets password with valid email and token (HTML)', async t => {
   const { web } = t.context;
-  const email = 'test5@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
 
-  const user = await Users.findOne({ email })
-    .select(config.userFields.resetToken)
-    .exec();
+  user = t.context.save.returnValues[0];
 
   if (!user) {
     throw new Error('User does not exist');
   }
 
+  t.context.findOne.resolves(user);
+  t.context.serialize.yields(null, { id: user.id });
+
   const res = await web
-    .post(`/en/reset-password/${user[global.config.userFields.resetToken]}`)
+    .post(`/en/reset-password/${user[config.userFields.resetToken]}`)
     .set({ Accept: 'text/html' })
     .send({ email, password });
 
   t.is(res.status, 302);
   t.is(res.header.location, '/en');
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('resets password with valid email and token (JSON)', async t => {
   const { web } = t.context;
-  const email = 'test6@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
 
-  const user = await Users.findOne({ email })
-    .select(global.config.userFields.resetToken)
-    .exec();
+  user = t.context.save.returnValues[0];
 
   if (!user) {
     throw new Error('User does not exist');
   }
 
+  t.context.findOne.resolves(user);
+  t.context.serialize.yields(null, { id: user.id });
+
   const res = await web
-    .post(`/en/reset-password/${user[global.config.userFields.resetToken]}`)
+    .post(`/en/reset-password/${user[config.userFields.resetToken]}`)
     .send({ email, password });
 
   t.is(res.status, 302);
   t.is(res.header.location, '/en');
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('fails resetting password for non-existent user', async t => {
   const { web } = t.context;
   const email = 'test7@example.com';
   const password = '!@K#NLK!#N';
+  t.context.findOne = sinon.stub(Users, 'findOne')
+    .resolves(null);
 
   const res = await web
     .post('/en/reset-password/randomtoken')
@@ -194,16 +260,24 @@ test('fails resetting password for non-existent user', async t => {
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.INVALID_RESET_PASSWORD);
+
+  t.context.findOne.restore();
 });
 
 test('fails resetting password with invalid reset token', async t => {
   const { web } = t.context;
-  const email = 'test8@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
+
+  t.context.findOne.resolves(null);
 
   const res = await web
     .post('/en/reset-password/wrongtoken')
@@ -211,114 +285,149 @@ test('fails resetting password with invalid reset token', async t => {
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.INVALID_RESET_PASSWORD);
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('fails resetting password with missing new password', async t => {
   const { web } = t.context;
-  const email = 'test9@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
 
-  const user = await Users.findOne({ email })
-    .select(global.config.userFields.resetToken)
-    .exec();
+  user = t.context.save.returnValues[0];
 
   if (!user) {
     throw new Error('User does not exist');
   }
 
   const res = await web
-    .post(`/en/reset-password/${user[global.config.userFields.resetToken]}`)
+    .post(`/en/reset-password/${user[config.userFields.resetToken]}`)
     .send({ email });
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.INVALID_PASSWORD);
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('fails resetting password with invalid email', async t => {
   const { web } = t.context;
-  const email = 'test10@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
 
-  const user = await Users.findOne({ email })
-    .select(global.config.userFields.resetToken)
-    .exec();
+  user = t.context.save.returnValues[0];
 
   if (!user) {
     throw new Error('User does not exist');
   }
 
   const res = await web
-    .post(`/en/reset-password/${user[global.config.userFields.resetToken]}`)
+    .post(`/en/reset-password/${user[config.userFields.resetToken]}`)
     .send({ email: 'wrongemail' });
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.INVALID_EMAIL);
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('fails resetting password with invalid email + reset token match', async t => {
   const { web } = t.context;
-  const email = 'test11@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
 
-  const user = await Users.findOne({ email }).exec();
+  user = t.context.save.returnValues[0];
 
   if (!user) {
     throw new Error('User does not exist');
   }
 
+  t.context.findOne.resolves(null);
+
   const res = await web
-    .post(`/en/reset-password/${user[global.config.userFields.resetToken]}`)
+    .post(`/en/reset-password/${user[config.userFields.resetToken]}`)
     .send({ email: 'wrongemail@example.com', password });
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.INVALID_RESET_PASSWORD);
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('fails resetting password if new password is too weak', async t => {
   const { web } = t.context;
-  const email = 'test12@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
 
-  const user = await Users.findOne({ email })
-    .select(global.config.userFields.resetToken)
-    .exec();
+  user = t.context.save.returnValues[0];
 
   if (!user) {
     throw new Error('User does not exist');
   }
 
+  t.context.findOne.resolves(user);
+
   const res = await web
-    .post(`/en/reset-password/${user[global.config.userFields.resetToken]}`)
+    .post(`/en/reset-password/${user[config.userFields.resetToken]}`)
     .send({ email, password: 'password' });
 
   t.is(res.status, 400);
   t.is(JSON.parse(res.text).message, phrases.INVALID_PASSWORD_STRENGTH);
+
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
 
 test('fails resetting password if reset was already tried in the last 30 mins', async t => {
   const { web } = t.context;
-  const email = 'test13@example.com';
+  let user = await factory.build('user');
+  const email = user.email;
   const password = '!@K#NLK!#N';
 
-  await web.post('/en/register').send({ email, password });
+  t.context.findOne = sinon.stub(Users, 'findOne');
+  t.context.findOne.resolves(user);
+  t.context.save = sinon.stub(Users.prototype, 'save')
+    .returnsThis();
 
   await web.post('/en/forgot-password').send({ email });
+
+  t.context.findOne.resolves(t.context.save.returnValues[0]);
 
   const res = await web.post('/en/forgot-password').send({ email });
 
@@ -327,12 +436,7 @@ test('fails resetting password if reset was already tried in the last 30 mins', 
     JSON.parse(res.text).message,
     util.format(phrases.PASSWORD_RESET_LIMIT, 'in 30 minutes')
   );
-});
 
-test('successfully logout', async t => {
-  const web = await login(t.context.web);
-  const res = await web.get('/en/logout');
-
-  t.is(res.header.location, '/en');
-  t.is(res.status, 302);
+  t.context.findOne.restore();
+  t.context.save.restore();
 });
